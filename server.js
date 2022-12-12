86270473a703bb1dea4688b85767400da63e8cfe
SeongUkMoon <46556494+SeongUkMoon@users.noreply.github.com>
2022-12-12 22:45:19 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+url = "https://f2ef24f7-0ee0-4-231-b9ee.documents.azure.com/dbs/Metadata/colls/items/docs/1"
+url = "https://f2ef24f7-0ee0-4-231-b9ee.documents.azure.com:443/dbs/Metadata/colls/items/docs/1"
+
+fetch(url).then(function(response){
+    return response.json();
+}).then(function(myJson){
+    console.log(JSON.stringify(myJson));
+});
