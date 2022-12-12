44cc51ae19598f83882f9a273d3b55c10083c2e2
SeongUkMoon <46556494+SeongUkMoon@users.noreply.github.com>
2022-12-13 00:50:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 url = "https://f2ef24f7-0ee0-4-231-b9ee.documents.azure.com/dbs/Metadata/colls/items/docs/1"
 url = "https://f2ef24f7-0ee0-4-231-b9ee.documents.azure.com:443/dbs/Metadata/colls/items/docs/1"
+url = "https://f2ef24f7-0ee0-4-231-b9ee.documents.azure.com:443
 
 fetch(url).then(function(response){
     return response.json();
